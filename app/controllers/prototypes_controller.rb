8c9bdf6cfd1ba4bdb6d1942aa61048365c404324
meita33 <124659672+meita33@users.noreply.github.com>
2023-02-27 18:28:32 +0900

    User・Prototypeモデル作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,3 +4,16 @@ class PrototypesController < ApplicationController
     @prototype = User.new[:name]
   end
 end
+
+def new
+  @prototype = Prototype.new
+end
+
+def create
+  @prototype = Prototype.new(prototype_params) 
+  if @prototype.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
